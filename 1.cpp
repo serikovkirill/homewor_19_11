fe6ea9043876a75bc8a1b87e0341ece3618ec14b
serikovkirill <91385926+serikovkirill@users.noreply.github.com>
2021-11-19 02:27:30 +0300

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -12,7 +12,7 @@ int main()
     }
     for (i = 0; i < n; i++)
     {
-        for (j = i + 1; j < n; j++)
+        for (j = i + 1; j < n; j++)//
         {
             if (s[i] > s[j])
             {
